16bc59c0a1627daff248f921e4b36d81d28da80a
peterliang233 <ncuyanping666@126.com>
2021-09-03 10:11:41 +0800

    :zap: 实现聊天室界面

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="view">
-    <el-container>
-      <el-header>简易聊天室</el-header>
-    </el-container>
-    <div v-for="(item,index) in msgList" :key="index" class="chat-box">{{item}}</div>
-    <div class="footer">
-      <el-input
-          style="float: left"
-          type="textarea"
-          :rows="2"
-          placeholder="请输入内容"
-          v-model="textarea">
-      </el-input>
-      <el-button type="primary" style="float: right" @click="send">发送/send</el-button>
+    <div id="div1">
+      <el-main>
+        <div v-for="(message,index) in messages">
+          <div v-if="index%2===0" class="item left">
+            <el-link style="font-size: 50px" icon="el-icon-user-solid"/>
+            <span class="message">{{ message.content }}</span>
+          </div>
+          <div v-if="index%2===1" class="item right">
+            <el-link style="font-size: 50px" icon="el-icon-user"/>
+            <span class="message">{{ message.content }}</span>
+          </div>
+        </div>
+      </el-main>
+    </div>
+    <div id="div2">
+      <div class="header">简易聊天室</div>
+      <el-footer class="footer">
+        <el-col :span="12" style="float: right">
+        <el-input
+            style="float: left;height: 50%;font-size:x-large"
+            type="textarea"
+            :rows="2"
+            placeholder="请输入内容"
+            v-model="textarea">
+        </el-input>
+        <el-button type="primary" style="float: right" @click="send">发送/send</el-button>
+        <el-upload
+            style="float: right"
+            class="upload-demo"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :before-remove="beforeRemove"
+            multiple
+            :limit="3"
+            :on-exceed="handleExceed"
+            :file-list="fileList">
+          <el-button type="primary">上传文件</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+          </el-col>
+      </el-footer>
     </div>
   </div>
 </template>
@@ -22,66 +51,145 @@ export default {
   name: 'Main',
   data() {
     return {
-      msg: '',
-      ws: '',
-      msgList: [],
-      textarea: ''
+      textarea: '',
+      messages: [
+        {
+          name: "xiaoliang",
+          content: "你好"
+        },
+        {
+          name: "xiaoyan",
+          content: "你也好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        },
+        {
+          name: "xiaoping",
+          content: "大家都好"
+        }
+      ]
     }
   },
   methods: {
-    send() {
-      this.ws.send(this.msg)
-      //this.ws.send(JSON.stringify({msg: this.msg}))
-      this.msg = ''
+    send: {
+
     }
-  },
-  mounted() {
-    this.ws = new WebSocket('ws://127.0.0.1:3000/ping')
-    // 连接打开时触发
-    this.ws.onopen = () => {
-      console.log("Connection open ...")
-    }
-    // 接收到消息时触发
-    this.ws.onmessage = (evt) => {
-      console.log(evt)
-      this.msgList.push(evt.data)
-    }
-    this.ws.onclose = () => {
-      console.log('Connection close !!!')
-    }
-  },
-  // 关闭连接
-  beforeDestroy() {
-    this.ws.close()
   }
 }
 </script>
 
 <style scoped>
-.view{
+#div1{
+  width:50%;
+  height:300px;
+  float:left;
+}
+#div2{
+  width:50%;
+  height:300px;
+  float:left;
+}
+.item {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.left {
+  flex-direction: row;
+}
+
+.right {
+  flex-direction: row-reverse;
+}
+
+.right .message {
+  margin-right: 10px;
+}
+
+.left .message {
+  margin-left: 10px;
+}
+
+.message {
+  border-radius: 10px;
+  display: flex;
+  background: #efefef;
+  min-height: 25px;
+  padding: 9px 10px;
+  align-items: center;
+  color: #222121;
+}
+
+.view {
   height: 100%;
   width: 100%;
-  position: fixed;
-  margin: 0 auto;
-  background-color:aliceblue;
+  background-color: aliceblue;
   text-align: center;
-  padding-top: 80px;
 }
-.chat-title{
-  text-align:left;
-  margin-left:100px;
-  margin-top:50px;
-}
-.chat-box{
-  background-color: white;
-  width: 400px;
-  margin: 0 auto;
-}
+
+
 .footer {
   position: fixed;
   left: 0;
   right: 0;
-  bottom: 0;
+  bottom: 95px;
+  padding: 20px;
+}
+
+.header {
+  position: fixed;
+  text-align: center;
+  left: 800px;
+  right: 0;
+  font-size: x-large;
+  font-weight: bolder;
+  top: 0;
   padding: 20px;
 }
 </style>
