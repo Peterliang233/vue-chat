139efbe4aa673adb0392d8fc9b8d27e153d1994f
peterliang233 <ncuyanping666@126.com>
2021-09-03 16:18:42 +0800

    :zap: 基本实现聊天界面

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,46 +1,50 @@
 <template>
   <div class="view">
     <div id="div1">
-      <el-main>
-        <div v-for="(message,index) in messages">
-          <div v-if="index%2===0" class="item left">
+      <div v-for="(message,index) in messages">
+        <div v-if="index%2===0" class="item left">
+          <div>
             <el-link style="font-size: 50px" icon="el-icon-user-solid"/>
-            <span class="message">{{ message.content }}</span>
+            <span>{{ message.name }}</span>
           </div>
-          <div v-if="index%2===1" class="item right">
-            <el-link style="font-size: 50px" icon="el-icon-user"/>
-            <span class="message">{{ message.content }}</span>
-          </div>
+          <span class="message">{{ message.content }}</span>
         </div>
-      </el-main>
+        <div v-if="index%2===1" class="item right">
+          <div>
+            <span>{{ message.name }}</span>
+            <el-link style="font-size: 50px" icon="el-icon-user"/>
+          </div>
+          <span class="message">{{ message.content }}</span>
+        </div>
+      </div>
     </div>
     <div id="div2">
       <div class="header">简易聊天室</div>
       <el-footer class="footer">
         <el-col :span="12" style="float: right">
-        <el-input
-            style="float: left;height: 50%;font-size:x-large"
-            type="textarea"
-            :rows="2"
-            placeholder="请输入内容"
-            v-model="textarea">
-        </el-input>
-        <el-button type="primary" style="float: right" @click="send">发送/send</el-button>
-        <el-upload
-            style="float: right"
-            class="upload-demo"
-            action="https://jsonplaceholder.typicode.com/posts/"
-            :on-preview="handlePreview"
-            :on-remove="handleRemove"
-            :before-remove="beforeRemove"
-            multiple
-            :limit="3"
-            :on-exceed="handleExceed"
-            :file-list="fileList">
-          <el-button type="primary">上传文件</el-button>
-          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
-          </el-col>
+          <el-input
+              style="float: left;height: 50%;font-size:x-large"
+              type="textarea"
+              :rows="2"
+              placeholder="请输入内容"
+              v-model="textarea">
+          </el-input>
+          <el-button type="primary" style="float: right" @click="send">发送/send</el-button>
+          <el-upload
+              style="float: left"
+              type="primary"
+              class="upload-demo"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              :before-remove="beforeRemove"
+              multiple
+              :limit="3"
+              :on-exceed="handleExceed"
+              :file-list="fileList">
+            <el-button type="primary" icon="el-icon-upload">上传文件</el-button>
+          </el-upload>
+        </el-col>
       </el-footer>
     </div>
   </div>
@@ -52,89 +56,31 @@ export default {
   data() {
     return {
       textarea: '',
-      messages: [
-        {
-          name: "xiaoliang",
-          content: "你好"
-        },
-        {
-          name: "xiaoyan",
-          content: "你也好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        },
-        {
-          name: "xiaoping",
-          content: "大家都好"
-        }
-      ]
+      messages: []
     }
   },
   methods: {
-    send: {
-
+    send() {
+      this.messages.push({name: 'xiaoliang', content: this.textarea});
+      this.textarea = '';
     }
   }
 }
 </script>
 
 <style scoped>
-#div1{
-  width:50%;
-  height:300px;
-  float:left;
+#div1 {
+  width: 50%;
+  height: 300px;
+  float: left;
 }
-#div2{
-  width:50%;
-  height:300px;
-  float:left;
+
+#div2 {
+  width: 50%;
+  height: 300px;
+  float: left;
 }
+
 .item {
   display: flex;
   margin-bottom: 10px;
